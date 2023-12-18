98ce598259baeeae5c2c91b6dcfbae0bea8dc726
Valentin <v.sourice@keyveo.com>
2023-12-18 22:10:35 +0100

    add npm i to launcher

--- launcher.bat
+++ launcher.bat
@@ -1,10 +1,10 @@
 @echo off
 
-start cmd /c "cd hub && npm run dev -- --port 5173"
-start cmd /c "cd goose && npm run dev -- --port 5174"
-start cmd /c "cd pokemon && npm run dev -- --port 5175"
-start cmd /c "cd memory && npm run dev -- --port 5176"
-start cmd /c "cd vampire && npm run dev -- --port 5177"
-start cmd /c "cd bomb && npm run dev -- --port 5178"
-start cmd /c "cd morpion && npm run dev -- --port 5179"
-start cmd /c "cd casino && npm run dev -- --port 5180"
+start cmd /c "cd hub && npm i && npm run dev -- --port 5173"
+start cmd /c "cd goose && npm i && npm run dev -- --port 5174"
+start cmd /c "cd pokemon && npm i && npm run dev -- --port 5175"
+start cmd /c "cd memory && npm i && npm run dev -- --port 5176"
+start cmd /c "cd vampire && npm i && npm run dev -- --port 5177"
+start cmd /c "cd bomb && npm i && npm run dev -- --port 5178"
+start cmd /c "cd morpion && npm i && npm run dev -- --port 5179"
+start cmd /c "cd casino && npm i && npm run dev -- --port 5180"
